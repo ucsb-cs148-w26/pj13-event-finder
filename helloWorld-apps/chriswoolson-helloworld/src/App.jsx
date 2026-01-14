a67a8f13fb0c5ef7f013472ff018aff75a494da7
ChrisWoolson <cw.woolson@gmail.com>
2026-01-14 12:00:00 -0800

    created helloworld app

---
+++ helloWorld-apps/chriswoolson-helloworld/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Welcome to ChrisWoolson's hello world app!
+      </div>
+      <button>Click me for event</button>
+    </>
+  )
+}
+
+export default App
