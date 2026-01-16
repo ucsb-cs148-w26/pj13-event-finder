c04b7241f81280aa5439386e2c0e4f7e09eea219
Wendy Song <wsong5885@gmail.com>
2026-01-16 13:07:14 -0800

    wendy helloworld

---
+++ helloWorld-apps/wendy-helloworld/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+	  Hello World!
+        </p>
+	<p>
+	  Wendy Song
+	</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
