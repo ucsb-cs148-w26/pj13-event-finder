dc3ea5d36fbd489796524dcbb7a550256515bdcb
Charles Zhang <charleszhang@ucsb.edu>
2026-01-21 12:01:20 -0800

    complete webapp with basic functionality

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="center-container">
+      <h1>Event Finder</h1>
+      <div className="button-wrapper">
+        <button>Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
